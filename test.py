320a72db2c98701813c3d2c10ebcecae525d593a
John Doe <johndory97@gmail.com>
2022-02-01 16:46:51 +0500

    ()_()

---
+++ test.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import pygame
+import requests
+
+coords = ",".join(input("Введите координаты через пробел: ").split())
+map_request = f"http://static-maps.yandex.ru/1.x/?ll={coords}&l=map&spn=0.2,0.2"
+response = requests.get(map_request)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+screen.blit(pygame.image.load(map_file), (0, 0))
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+os.remove(map_file)
