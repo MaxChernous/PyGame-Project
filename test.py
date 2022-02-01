10ae00de5ac867f8282be2752f659e9f98b4d481
petrenkosofya <Sofi20102005@gmail.com>
2022-02-01 17:56:23 +0500

    Обновление вынесено в функцию, вызывается не каждый цикл

--- test.py
+++ test.py
@@ -3,12 +3,29 @@ import sys
 import pygame
 import requests
 
+def change(coords_list, map_file, z):
+    coords = ",".join(coords_list)
+    map_request = f"http://static-maps.yandex.ru/1.x/?ll={coords}&l=map&z={z}"
+    response = requests.get(map_request)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        return False
+
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    return True
+
+WINDOW_WIDTH, WINDOW_HEIGHT = 600, 450
 z = 6
+map_file = "map.png"
+coords_list = input("Введите координаты через пробел: ").split()
+change(coords_list, map_file, z)
 
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
-coords = ",".join(input("Введите координаты через пробел: ").split())
-map_request = f"http://static-maps.yandex.ru/1.x/?ll={coords}&l=map&z={z}"
 running = True
 while running:
     for event in pygame.event.get():
@@ -18,25 +35,13 @@ while running:
             if event.key == pygame.K_UP:
                 if z <= 18:
                     z += 1
+                    change(coords_list, map_file, z)
             if event.key == pygame.K_DOWN:
                 if z >= 4:
                     z -= 1
+                    change(coords_list, map_file, z)
 
-    map_request = f"http://static-maps.yandex.ru/1.x/?ll={coords}&l=map&z={z}"
-    response = requests.get(map_request)
-
-    if not response:
-        print("Ошибка выполнения запроса:")
-        print(map_request)
-        print("Http статус:", response.status_code, "(", response.reason, ")")
-        sys.exit(1)
-
-    else:
-        map_file = "map.png"
-        with open(map_file, "wb") as file:
-            file.write(response.content)
-        screen.blit(pygame.image.load(map_file), (0, 0))
-
+    screen.blit(pygame.image.load(map_file), (0, 0))
     pygame.display.flip()
 
 pygame.quit()
